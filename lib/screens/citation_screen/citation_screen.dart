68dae81689f13870b94980d59fe5ee65cfa6bc80
akhaled <akhaled@cairo.aas.com.sa>
2022-04-04 17:36:39 +0200

    handle citation screen again

--- lib/screens/citation_screen/citation_screen.dart
+++ lib/screens/citation_screen/citation_screen.dart
@@ -1,12 +1,9 @@
-import 'dart:ui';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_apps/data/models/citation_model.dart';
 import 'package:flutter_apps/data/repos/citation_repo.dart';
 import 'package:flutter_apps/screens/citation_screen/widgets/app_bar_container.dart';
 import 'package:flutter_apps/screens/citation_screen/widgets/citation_container.dart';
 import 'package:flutter_apps/screens/citation_screen/widgets/citation_counter.dart';
-import 'package:flutter_apps/screens/citation_screen/widgets/dialog_container.dart';
 import 'package:flutter_apps/screens/citation_screen/widgets/navigation_button.dart';
 import 'package:flutter_apps/utilities/constants/app_colors.dart';
 
@@ -16,8 +13,8 @@ class CitationScreen extends StatefulWidget {
       : super(key: key);
 
   @override
-  // ignore: no_logic_in_create_state
   State<CitationScreen> createState() =>
+      // ignore: no_logic_in_create_state
       _CitationScreenState(currentCitation: currentCitation);
 }
 
@@ -26,63 +23,12 @@ class _CitationScreenState extends State<CitationScreen> {
   _CitationScreenState({required this.currentCitation});
   late List<Citation> citations;
   int citationNumber = 0;
-
-  void nextCitation() {
-    if (citationNumber >= 0 && citationNumber < citations.length - 1) {
-      setState(() {
-        citationNumber += 1;
-      });
-    } else {
-      showDialog(
-        context: context,
-        barrierDismissible: false, // user must tap button!
-        builder: (context) {
-          return const DialogContainer();
-        },
-      );
-    }
-  }
-
-  void previousCitation() {
-    if (citationNumber < 0 && citationNumber >= citations.length - 1) {
-      setState(() {
-        citationNumber -= 1;
-      });
-    } else {
-      showDialog(
-        context: context,
-        barrierDismissible: false, // user must tap button!
-        builder: (context) {
-          return AlertDialog(
-            title: const Text(
-              'لقد وصلت لبداية الأذكار',
-              textDirection: TextDirection.rtl,
-            ),
-            actions: [
-              TextButton(
-                onPressed: () {
-                  Navigator.of(context, rootNavigator: true).pop('dialog');
-                },
-                child: const Text(
-                  'حسناً',
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 20.0,
-                    color: kPrimaryColor,
-                  ),
-                ),
-              ),
-            ],
-          );
-        },
-      );
-    }
-  }
+  int completeNumber = 0;
 
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
+    completeNumber = 0;
     getCitations().then(
       (value) {
         setState(() {
@@ -97,6 +43,14 @@ class _CitationScreenState extends State<CitationScreen> {
         await getCurrentCitation(citation: currentCitation);
 
     return tempCitations;
+  }
+
+  final PageController _pageController = PageController();
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
   }
 
   @override
@@ -126,90 +80,91 @@ class _CitationScreenState extends State<CitationScreen> {
                   fit: BoxFit.cover,
                 ),
               ),
-              child: Column(
-                //  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  CitationContainer(
-                    citationText: citations[citationNumber].zekr,
-                    isReference: false,
-                  ),
-                  const Divider(
-                    height: 5,
-                    color: kPrimaryColor,
-                    indent: 10,
-                    endIndent: 10,
-                    thickness: 2,
-                  ),
-                  CitationContainer(
-                    citationText: citations[citationNumber].description,
-                    isReference: true,
-                  ),
-                  CitationContainer(
-                    citationText: citations[citationNumber].reference,
-                    isReference: true,
-                  ),
-                  const Spacer(),
-                  Padding(
-                    padding: EdgeInsets.only(
-                      bottom: size.height * 0.1,
-                      left: 10.0,
-                      right: 10.0,
-                      top: 10.0,
-                    ),
-                    child: Column(
-                      children: [
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceAround,
-                          children: [
-                            NavigationButton(
-                              title: 'السابق',
-                              onPress: () {
-                                print('previous');
-                                previousCitation();
-                              },
-                              isNext: false,
+              child: PageView.builder(
+                controller: _pageController,
+                itemCount: citations.length,
+                itemBuilder: (
+                  BuildContext context,
+                  int index,
+                ) {
+                  citationNumber = index;
+                  return CustomScrollView(
+                    shrinkWrap: true,
+                    slivers: [
+                      SliverList(
+                        delegate: SliverChildListDelegate(
+                          [
+                            CitationContainer(
+                              citationText: citations[citationNumber].reference,
+                              isReference: true,
                             ),
-                            CitationCounter(
-                              count: citations[citationNumber].count.toString(),
+                            CitationContainer(
+                              citationText: citations[index].zekr,
+                              isReference: false,
                             ),
-                            NavigationButton(
-                              isNext: true,
-                              title: 'التالي',
-                              onPress: () {
-                                print('next');
-                                nextCitation();
-                              },
+                            const Divider(
+                              height: 5,
+                              color: kPrimaryColor,
+                              indent: 10,
+                              endIndent: 10,
+                              thickness: 2,
+                            ),
+                            CitationContainer(
+                              citationText:
+                                  citations[citationNumber].description,
+                              isReference: true,
                             ),
                           ],
                         ),
-                        const SizedBox(
-                          height: 20.0,
-                        ),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceAround,
-                          children: [
-                            const Text(
-                              'عدد الأذكار',
-                              style: TextStyle(
-                                color: kPrimaryColor,
-                                fontWeight: FontWeight.bold,
-                                fontSize: 20.0,
-                              ),
+                      ),
+                      SliverFillRemaining(
+                        hasScrollBody: false,
+                        child: Align(
+                          alignment: Alignment.bottomCenter,
+                          child: Padding(
+                            padding: EdgeInsets.only(
+                              bottom: size.height * 0.15,
+                              left: 10.0,
+                              right: 10.0,
+                              top: 10.0,
                             ),
-                            Text(
-                              '$citationNumber / ${citations.length} ',
-                              style: const TextStyle(
-                                color: kPrimaryColor,
-                                fontWeight: FontWeight.bold,
-                                fontSize: 20.0,
-                              ),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceAround,
+                              children: [
+                                NumberContainer(
+                                  title: '${citations[citationNumber].count}',
+                                  isLeft: false,
+                                ),
+                                CitationCounter(
+                                  count: completeNumber.toString(),
+                                  onPress: () {
+                                    setState(() {
+                                      completeNumber += 1;
+                                      if (completeNumber ==
+                                          citations[citationNumber].count) {
+                                        _pageController.nextPage(
+                                          duration:
+                                              const Duration(milliseconds: 400),
+                                          curve: Curves.easeInOut,
+                                        );
+                                        completeNumber = 0;
+                                      }
+                                    });
+                                  },
+                                ),
+                                NumberContainer(
+                                  isLeft: true,
+                                  title:
+                                      '${citationNumber + 1} / ${citations.length} ',
+                                ),
+                              ],
                             ),
-                          ],
+                          ),
                         ),
-                      ],
-                    ),
-                  ),
-                ],
+                      ),
+                    ],
+                  );
+                },
               ),
             ),
           ),
